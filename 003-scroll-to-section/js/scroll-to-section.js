bd4af634df27ae1ad4324409771aaf2dbb21f33f
hyeonah <hyeonah.hello@gmail.com>
2022-01-06 15:59:56 +0900

    Add comments

--- 003-scroll-to-section/js/scroll-to-section.js
+++ 003-scroll-to-section/js/scroll-to-section.js
@@ -1,18 +1,24 @@
+// variable declaration
 const menus = document.querySelectorAll('header a');
 const underline = document.getElementById('underline');
 const sections = document.querySelectorAll('section');
 let secOffsetTop;
 
+// window starts from position(0, 0) wherever it's loaded
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
 
+// when window is loaded, execute displayUnderline function passing parameter 0
+// parameter 0 will be used for displaying underline on first menu
 window.onload = displayUnderline(0);
 
+// when each menu is clicked
+// 1. execute displayUnderline function
+// 2. scroll to section
 menus.forEach((menu, index) => {
   menu.addEventListener('click', function (e) {
     e.preventDefault;
-    console.log(index);
     displayUnderline(index);
     secOffsetTop = sections[index].offsetTop;
     window.scrollTo({
@@ -22,6 +28,7 @@ menus.forEach((menu, index) => {
   });
 });
 
+// Underline will be moved to the menu accordingly
 function displayUnderline(index) {
   underline.style.left = menus[index].offsetLeft + 'px';
   underline.style.width = menus[index].offsetWidth + 'px';
